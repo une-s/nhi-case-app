4db28199ee543ca45b5020cb341ff1c618af1aec
Une <36519297+une-s@users.noreply.github.com>
2022-11-07 17:08:05 +0100

    Enable routing, fetch user on detail page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Header from './common/components/Header';
 import Footer from './common/components/Footer';
 import Home from './pages/home';
+import Details from './pages/details';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/:username" element={<Details />} />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,1 +1,39 @@
-export default null;
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import config from 'config.json';
+import User from 'models/User';
+
+function Details() {
+
+  const [user, setUser] = useState<User|null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const { username } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const options = { headers: {
+      accept: "application/vnd.github+json"
+    }};
+    fetch(`${config.api.baseUrl}/users/${username}`, options)
+      .then((response) => response.status === 200 ? response.json() : null)
+      .then((data) => {
+        setUser(data !== null ? new User(data) : null);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+  else if (user === null) {
+    return <div>That user doesn't exist</div>;
+  }
+  return (
+    <div>{user.login}</div>
+  );
+}
+
+export default Details;
