adad9f95c08204e6a675362b8fac0fedccd13804
Une <36519297+une-s@users.noreply.github.com>
2022-11-07 23:50:02 +0100

    style footer

--- src/common/components/footer/index.tsx
+++ src/common/components/footer/index.tsx
@@ -1,6 +1,8 @@
+import styles from './style.module.css';
+
 function Footer() {
   return (
-    <footer className="page-footer">
+    <footer className={styles.footer}>
       <p>&copy; Une Simonsen 2022</p>
     </footer>
   );
