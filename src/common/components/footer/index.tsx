b30fa17b39c5078b7fcb790feaaf4c77c3aee4b7
Une <36519297+une-s@users.noreply.github.com>
2022-11-08 01:41:33 +0100

    Fix footer text

--- src/common/components/footer/index.tsx
+++ src/common/components/footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; Une Simonsen 2022</p>
+      <p>&copy; 2022 Une Simonsen</p>
     </footer>
   );
 }
