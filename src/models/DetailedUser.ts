a7c2c349543279b3f6fa8477efb8b2b930b8b0d1
Une <36519297+une-s@users.noreply.github.com>
2022-11-08 11:38:49 +0100

    Add to user detail

--- src/models/DetailedUser.ts
+++ src/models/DetailedUser.ts
@@ -3,6 +3,7 @@ import { validateJson } from 'common/util/validators';
 
 class DetailedUser extends User {
 
+  name: string;
   githubUrl: string;
   followers: number;
   following: number;
@@ -13,15 +14,22 @@ class DetailedUser extends User {
 
   constructor(json: any) {
     super(json);
-    const requiredProps:string[] = ['html_url', 'followers', 'following'];
-    const requiredTypes:string[] = ['string', 'number', 'number'];
+    const requiredProps:string[] = ['name', 'html_url', 'followers', 'following'];
+    const requiredTypes:string[] = ['string', 'string', 'number', 'number'];
 
     // May throw TypeError
     validateJson(json, requiredProps, requiredTypes);
 
+    this.name = json.name;
     this.githubUrl = json.html_url;
     this.followers = json.followers;
     this.following = json.following;
+    this.bio = json.bio;
+    this.company = json.company;
+    this.email = json.email;
+    this.blogUrl = json.blog && !json.blog.match(/^https?:\/\//) ?
+      'https://' + json.blog :
+      json.blog;
   }
 }
 export default DetailedUser;
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import apiConfig from 'settings/apiConfig.json';
 import DetailedUser from 'models/DetailedUser';
+import styles from './style.module.css';
 
 function Details() {
 
@@ -22,16 +23,38 @@ function Details() {
   }, [username]);
 
   return (
-    <div>
+    <>
+    <div className={styles.wrapper}>
       {
         isLoading ? "Loading..." :
         user === null ? "That user doesn't exist" :
         <>
-          <h1>{user.username}</h1>
-          <img src={user.avatarUrl} alt="avatar" />
+          <section className={styles.profile}>
+            <img className={styles.avatar} src={user.avatarUrl} alt="avatar" />
+            <h1>{user.name}</h1>
+            <h2>{user.username}</h2>
+            <ul>
+              <li>Followers: {user.followers}</li>
+              <li>Following: {user.following}</li>
+            </ul>
+            <h2>Websites</h2>
+            <ul>
+              <li><a href={user.githubUrl} target="_blank" rel="noreferrer">
+                Github Page
+              </a></li>
+              {
+                user.blogUrl ?
+                  <li><a href={user.blogUrl} target="_blank" rel="noreferrer">
+                    Blog Website
+                  </a></li> :
+                  null
+              }
+            </ul>
+          </section>
         </>
       }
     </div>
+    </>
   );
 }
 
