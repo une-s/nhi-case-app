840038471a15439674b65bf8686ac92d117eee51
Une <36519297+une-s@users.noreply.github.com>
2022-11-09 21:47:19 +0100

    improve header

--- src/common/components/header/index.tsx
+++ src/common/components/header/index.tsx
@@ -1,18 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { BsArrowLeftCircle } from "react-icons/bs";
 import config from 'settings/appConfig.json';
 import styles from './style.module.css';
 
 function Header() {
+
+  const location = useLocation();
+
   return (
-    <header className={styles.header}>
-      <div className={styles.headerInner}>
-        <div className={styles.brand}>
-          <Link to="/">
-            <p className={styles.brandTitle}>{config.name}</p>
-          </Link>
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerInner}>
+          <div className={styles.brand}>
+            <Link to="/">
+              <p className={styles.brandTitle}>{config.name}</p>
+            </Link>
+          </div>
+          { location.pathname !== "/" ?
+            <div className={styles.backLink} title="Back">
+              <Link to="/"><BsArrowLeftCircle /></Link>
+            </div> :
+            null }
         </div>
-      </div>
-    </header>
+      </header>
+      <div className={styles.behindHeader}></div>
+    </>
   );
 }
 export default Header;
