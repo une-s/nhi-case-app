0da24cde9bb6fed6010ea3f0a7a37a6c61a13664
Une <36519297+une-s@users.noreply.github.com>
2022-11-10 10:22:16 +0100

    Add some repo stats

--- src/models/Repository.ts
+++ src/models/Repository.ts
@@ -5,11 +5,13 @@ class Repository {
   id: number;
   name: string;
   fullName: string;
+  stars: number;
+  forks: number;
   description?: string;
 
   constructor(json: any) {
-    const requiredProps:string[] = ['id', 'name', 'full_name'];
-    const requiredTypes:string[] = ['number', 'string', 'string'];
+    const requiredProps:string[] = ['id', 'name', 'full_name', 'stargazers_count', 'forks_count'];
+    const requiredTypes:string[] = ['number', 'string', 'string', 'number', 'number'];
 
     // May throw TypeError
     validateJson(json, requiredProps, requiredTypes);
@@ -17,6 +19,8 @@ class Repository {
     this.id = json.id;
     this.name = json.name;
     this.fullName = json.full_name;
+    this.stars = json.stargazers_count;
+    this.forks = json.forks_count;
     this.description = json.description;
   }
 }
--- src/pages/details/components/repo-list-item/index.tsx
+++ src/pages/details/components/repo-list-item/index.tsx
@@ -1,3 +1,5 @@
+import { BsStar } from 'react-icons/bs';
+import { BiGitRepoForked } from 'react-icons/bi';
 import Repository from 'models/Repository';
 import styles from './style.module.css';
 
@@ -8,7 +10,13 @@ interface RepoListItemProps {
 function RepoListItem({repo}:RepoListItemProps) {
   return (
     <li className={styles.repoListItem}>
-      <h3 className={styles.title}>{repo.name}</h3>
+      <header className={styles.header}>
+        <h3 className={styles.title}>{repo.name}</h3>
+        <span className={styles.stats}>
+          <span className={styles.stars}><BsStar />{repo.stars}</span>
+          <span className={styles.forks}><BiGitRepoForked />{repo.forks}</span>
+        </span>
+      </header>
       <div className={styles.itemBody}>
         { repo.description ?
           <p className={styles.description}>{repo.description}</p> :
